39a1cbf56106be851b6a9b88d34c1190670df20a
cykecho <61276192+cykecho@users.noreply.github.com>
2020-12-01 00:20:14 +0800

    完成登录、用户主界面

---
+++ src/components/Home.vue
@@ -0,0 +1,159 @@
+<template>
+    <div class="container" style="background-color: #eee;">
+        <div class="page">
+            <div class="blank-bar"></div>
+            <el-row :gutter="20">
+                <el-col :span="4">
+                    <div class="userInfo">
+                        <div class="userInfo-top"></div>
+                        <div class="userInfo-name">
+                            <div class="avatar-box">
+                                <img src="../assets/img/avatar.jpg" alt />
+                            </div>
+                            <div @click="goClass">江小博</div>
+                            <div style="font-size:13px">欢迎来到网络课堂!</div>
+                        </div>
+                        <div class="userInfo-school">
+                            <div class="key">学校</div>
+                            <div class="value">江苏大学</div>
+                        </div>
+                        <div class="userInfo-grade">
+                            <div class="key">年级</div>
+                            <div class="value">2018级</div>
+                        </div>
+                        <div class="userInfo-major">
+                            <div class="key">专业</div>
+                            <div class="value">软件工程</div>
+                        </div>
+                        <div class="userInfo-id">
+                          <div class="key">学号</div>
+                          <div class="value">3180608001</div>
+                        </div>
+                        <div class="user-logout">
+                          <el-button type="danger" plain @click="logout()">退出</el-button>
+                        </div>
+                    </div>
+                </el-col>
+                <el-col :span="13">
+                    <Timetable></Timetable>
+                </el-col>
+                <el-col :span="6">
+                    <Notice></Notice>
+                </el-col>
+            </el-row>
+        </div>
+    </div>
+</template>
+
+<script>
+import { MessageBox } from 'element-ui'
+export default {
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    },
+    goClass () {
+      MessageBox.alert('《Java程序设计》将在五分钟后上课，请尽快进入课堂', '上课提醒', {
+        confirmButtonText: '跳转进入',
+        callback: action => {
+          if (action) {
+            this.$router.push('/livebroadcast')
+          }
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.color {
+  background-color: #999;
+  height: 90px;
+  margin: 5px 10px;
+}
+.page {
+  width: 1400px;
+  height: 100%;
+  margin: 0 auto;
+  background-color: white;
+  .blank-bar {
+    height: 20px;
+  }
+  .userInfo {
+    margin-left: 30px;
+    height:500px;
+    box-shadow: 2px 1px 5px #999;
+    border-radius: 20px;
+    display: flex;
+    flex-direction: column;
+    font-size: 13px;
+    .key {
+      color: #999;
+      padding-top: 10px;
+    }
+    .value {
+      padding-top: 5px;
+    }
+    .userInfo-top {
+      // flex:1;
+      height: 60px;
+      background-color: #015478;
+      border-radius: 20px 20px 0 0;
+    }
+    .userInfo-name {
+        // flex:1.5;
+        height: 90px;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+        font-size: 15px;
+        .avatar-box {
+            height: 90px;
+            width: 90px;
+            border: 1px solid #eee;
+            border-radius: 50%;
+            // box-shadow: 0 0 10px;
+            position: relative;
+            left: 50%;
+            transform: translate(-50%, 0%);
+            img {
+              width: 100%;
+              height: 100%;
+              border-radius: 50%;
+            }
+        }
+        div {
+          text-align: center;
+          line-height: 25px;
+        }
+    }
+    .userInfo-name,
+    .userInfo-school,
+    .userInfo-grade,
+    .userInfo-major {
+      border-bottom: solid 1px #eee;
+    }
+    .userInfo-school,
+    .userInfo-grade,
+    .userInfo-major,
+    .userInfo-id {
+      height: 55px;
+    }
+    .userInfo-school,
+    .userInfo-grade,
+    .userInfo-major,
+    .userInfo-id {
+      padding-left: 20px;
+      display: flex;
+      flex-direction: column;
+    }
+    .user-logout{
+    position: relative;
+    top: 40px;
+    left: 30%;
+    }
+  }
+}
+</style>
---
+++ src/components/Timetable.vue
@@ -0,0 +1,136 @@
+<template>
+    <div class="class-table">
+        <div class="table-wrapper">
+            <div class="tabel-container">
+                <table>
+                    <thead>
+                        <tr>
+                            <th style="border-radius:20px 0 0 0">时间</th>
+                            <th>周一</th>
+                            <th>周二</th>
+                            <th>周三</th>
+                            <th>周四</th>
+                            <th>周五</th>
+                            <th>周六</th>
+                            <th style="border-radius:0 20px 0 0">周日</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(lesson, lessonIndex) in classTableData.lessons" :key="lessonIndex">
+                            <td>
+                                <p class="period">{{ lesson }}</p>
+                            </td>
+                            <td v-for="(course, courseIndex) in classTableData.courses"
+                            :class="classTableData.courses[courseIndex][lessonIndex]?'active':''" :key="courseIndex">
+                                {{classTableData.courses[courseIndex][lessonIndex] || ' '}}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      classTableData: {
+        lessons: [
+          '08:00-08:45',
+          '08:55-09:40',
+          '10:30-11:15',
+          '11:15-12:10',
+          '14:00-14:45',
+          '14:55-15:40',
+          '16:00-16:45',
+          '16:55-17:40',
+          '19:00-19:45',
+          '19:55-20:40',
+          '20:50-21:35'
+        ],
+        courses: [
+          ['计算方法（计算机楼109）', '计算方法（计算机楼109）', '计算机网络（计算机楼106）', '计算机网络（计算机楼106）', '算法设计与分析（计算机楼110）', '算法设计与分析（计算机楼110）', '', '', 'JAVA EE上机实验（计算机楼306）', 'JAVA EE上机实验（计算机楼306）', ''],
+          ['操作系统（计算机楼109）', '操作系统（计算机楼109）', 'JAVA EE与中间件技术（计算机楼106）', 'JAVA EE与中间件技术（计算机楼106）', '操作系统上机实验（计算机楼306）', '操作系统上机实验（计算机楼306）', '', '', '汽车交通与安全（三山楼109）', '汽车交通与安全（三山楼109）', ''],
+          ['数字图像处理（计算机楼109）', '数字图像处理（计算机楼109）', '计算机网络（计算机楼106）', '计算机网络（计算机楼106）', '', '', '', '', '', '计算机网络上机实验（计算机楼305）', '计算机网络上机实验（计算机楼305）'],
+          ['需求工程：软件建模与分析（计算机楼109）', '需求工程：软件建模与分析（计算机楼109）', '数据结构（三山楼305）', '数据结构（三山楼305）', '', '', '', '', '', '', ''],
+          ['操作系统（计算机楼106）', '操作系统（计算机楼106）', '', '', 'JAVA EE与中间件技术（计算机楼106）', 'JAVA EE与中间件技术（计算机楼106）', '', '', '', '', ''],
+          ['', '', '创业计划（讲堂群103）', '创业计划（讲堂群103）', '', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', '', '']
+        ]
+      }
+
+    }
+  },
+  created () {},
+  methods: {
+    /**
+        * 数字转中文
+        * @param {Number} num 需转数字
+        * @param {String} identifier 标识符
+        * @returns {String} 转换后的中文
+        */
+    digital2Chinese (num, identifier) {
+      const character = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '十一', '十二']
+      return identifier === 'week' && (num === 0 || num === 7) ? '日' : character[num]
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.class-table {
+    .table-wrapper {
+        width: 100%;
+        // height: 100%;
+        overflow: auto;
+    }
+    .active {
+        color: white !important;
+        background-color: #388FB4;
+        font-size:12px !important;
+        cursor: pointer;
+    }
+    .tabel-container {
+        margin: 0 10px;
+
+        table {
+            table-layout: fixed;
+            width: 100%;
+
+            thead {
+                background-color: #015478;
+                th {
+                    color: #fff;
+                    font-weight: 400;
+                    font-size:16px;
+                }
+            }
+            tbody {
+                background-color: #eaf2ff;
+                td {
+                    color: #677998;
+                }
+            }
+            th,
+            td {
+                width: 60px;
+                padding: 10px 2px;
+                font-size: 14px;
+                text-align: center;
+            }
+
+            tr td:first-child {
+                color: #333;
+                .period {
+                    font-size: 13px;
+                    font-weight: 500;
+                }
+            }
+        }
+    }
+}
+</style>
